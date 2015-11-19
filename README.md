117f9729ec8ff3ff1a3951515fa9c6dd5ad0a6a0
Sebastian Sulinski <Sebastian@Sebastians-Mac-Pro.local>
2015-11-19 17:14:06 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# link-exchange-system
-Exercise files for the course Link Exchange System with Dreamweaver, PHP, MySQL and CSS
+# Link Exchange System with Dreamweaver, PHP, MySQL and CSS
+Exercise files for the course **Link Exchange System with Dreamweaver, PHP, MySQL and CSS**
---
+++ start/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Title</title>
+<meta name="description" content="" />
+<meta name="keywords" content="" />
+<meta http-equiv="imagetoolbar" content="no" />
+</head>
+<body>
+
+<div id="wrapper">
+	<div id="header">
+		<p>your sites url (example: www.sebastiansulinski.co.uk)</p>
+	</div>
+	<div id="container">
+	
+		<!-- Content goes here -->
+		
+		<!-- /Content goes here -->
+	
+	</div>
+	<div id="footer">
+		<p>&copy; <a href="http://www.sebastiansulinski.co.uk/" title="Web Designer East Sussex" target="_blank">
+			Web Designer East Sussex</a>
+		</p>
+	</div>
+</div>
+
+</body>
+</html>
